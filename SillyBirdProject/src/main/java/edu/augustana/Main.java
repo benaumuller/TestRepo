3829b03756b5486b216524eb7017a863a057bd0d
benaumuller <bloonboy13@gmail.com>
2023-09-27 20:55:18 -0500

    Now dancing with chicken

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -2,6 +2,8 @@ package edu.augustana;
 
 public class Main {
     public static void main(String[] args) {
+        Chicken chicken = new Chicken();
+        chicken.dance();
         System.out.println("Hello bird world!");
     }
 }
